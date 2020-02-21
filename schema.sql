0fe73958daabb84e47dd9958198eb9682b41ca28
Colin Cunningham <colin.199643@gmail.com>
2020-02-21 11:18:22 -0500

    Created a working database using the view technique in SQL, will continue to add more functionality as I go along.

--- schema.sql
+++ schema.sql
@@ -1,28 +1,81 @@
-DROP DATABASE IF EXISTS company_data;
-CREATE database company_data;
+DROP DATABASE IF EXISTS companyData_db;
+CREATE database companyData_db;
 
-USE company_data;
+USE companyData_db;
 
 CREATE TABLE Employee (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30), 
   role_id INT,
-  manager_id INT 
+  manager_id INT,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE Role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
+    id INT NOT NULL,
+    title VARCHAR(80),
     salary DECIMAL(10,4) NULL,
     department_id INT,
-)
+     PRIMARY KEY(id)
+);
 
 CREATE TABLE department (
-     id INT PRIMARY KEY,
-     name VARCHAR(30),
-)
+     id INT AUTO_INCREMENT NOT NULL,
+     name VARCHAR(30)
+);
+
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES("Michael", "Scott", 1, NULL),("Jim", "Halpert", 2, 1),("Angela", "Martin",7, 1),
+("Kevin","Malone", 7, 1),("Oscar","Nunez",6, 1),("Dwight", "Schrute", 3, 2), ("Stanley","Hudson", 5, 1),
+("Phyllis" "Smith", 6, 1),("Ryan", "Howard", 10, NULL);
+
+
+-- Roles
+
+INSERT INTO Role (id, title, salary, department_id)
+VALUES(1, "Manager", 100000, 1),(2, "Co-Manager", 80000, 1),(3, "Assitant to the Regional Manager", 70000, 3),
+(4, "Human Resources", 60000, 2),(5, "Salesmen", 40000, 3),(6, "SalesLead", 40000, 3),(6, "Head Accountant", 60000, 4),
+(7, "Accountant", 50000, 4),(8, "Customer-Service", 60001, 2),(9, "Forklift Operator", 70000, 5),(10, "Corporate", 100000, 1);
+
+INSERT INTO department (id, name)
+VALUES(1, "Managment"),(2, "HR"),(3, "Sales"),(4, "Accounting"),(5, "Warehouse")
+
+CREATE VIEW Employee_publictest AS
+SELECT first_name, last_name 
+FROM Employee
+
+CREATE VIEW Employee_data AS
+SELECT * FROM Employee INNER JOIN Role 
+ON Employee.role_id = Role.id_r
+;
 
 
 
-SELECT * FROM Employee;
+-- All Query
+SELECT first_name, last_name, title, salary FROM Employee INNER JOIN Role 
+ON Employee.role_id = Role.id;
+
+--
+
+CREATE VIEW Employee_data AS
+SELECT * FROM Employee INNER JOIN Role 
+ON Employee.role_id = Role.id_r
+;
+
+CREATE VIEW EmployeeName AS
+SELECT id, first_name, last_name FROM Employee 
+;
+SELECT * FROM EmployeeName;
+
+--
+CREATE VIEW Employee_public AS
+SELECT first_name,last_name, title, salary, department FROM Employee_data INNER JOIN department 
+ON Employee_data.department_id = department.id_d;
+
+--Manager View
+CREATE VIEW view_manager AS
+SELECT Employee_data.id, Employee_data.first_name,Employee_data.last_name, 
+concat(EmployeeName.first_name, ' ', EmployeeName.last_name) AS 'Manager'
+FROM Employee_data INNER JOIN EmployeeName ON Employee_data.manager_id = EmployeeName.id;
+
--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const art = require("ascii-art");
+// const art = require("ascii-art");
 const table = require("console.table");
+const Employee = require("./lib/Employee")
+const prompts = require("./lib/prompts")
+const query = require("./lib/query")
 
-//Module Imports//
-const employee =  require(".Employee")
-const department = require(".Department")
-const role = require(".Role")
-
-
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -20,6 +17,83 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "Rastler1234!",
-  database: "topSongs_db"
+  database: "companyData_db"
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  getAnswer();
+});
+
+// Main Function //
+function getAnswer() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all Employees",
+          "View all Employees by Department",
+          "View all Employees by Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager"
+        ],
+        name: "action"
+      }
+    ])
+    .then(function(res) {
+      switch (res.action) {
+        case "View all Employees":
+          query.displayAll();
+          getAnswer()
+          break;
+      }
+      switch (res.action) {
+        case "View all Employees by Department":
+          prompts.displayDep()
+          break;
+      }
+      switch (res.action) {
+        case "View all Employees by Manager":
+         prompts.displayMan()
+      
+        break;
+      }
+
+      switch (res.action) {
+        case "Add Employee":
+          break;
+      }
+      switch (res.action) {
+        case "Remove Employee":
+          break;
+      }
+      switch (res.action) {
+        case "Update Employee Role":
+          break;
+      }
+      switch (res.action) {
+        case "Update Employee Manager":
+          break;
+      }
+    });
+}
+
+
+function displayAll() {
+  connection.query(
+    "SELECT First_name, Last_name FROM Employee LEFT JOIN Role ON Employee.role_id = Role.id",
+
+    function(err, res) {
+      if (err) throw err;
+      console.table(res);
+    }
+  );
+}
+
+
+  
